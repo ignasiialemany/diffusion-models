e3b1462938e48b2401eed9eacba494533338e70e
ignasiialemany <59746390+ignasiialemany@users.noreply.github.com>
2020-08-11 18:58:53 +0200

    Fixed bug in pos_reflect and pos_pass

--- randomWalk/MonteCarlo.py
+++ randomWalk/MonteCarlo.py
@@ -52,7 +52,8 @@ class MonteCarlo:
         self.position += step
         newIndices = self.locateIndex()
         interacting = self.indices != newIndices
-        if np.any(interacting):
+        if np.any(interacting == True):
+            initialPosition = initPos[interacting]
             pos_interact = self.position[interacting]
             #Correct default value when walkers are out of the domain
             newIndices[np.where((newIndices == -1) & (self.position>self.domain.length))] = self.domain.numberOfCompartments-1
@@ -62,9 +63,8 @@ class MonteCarlo:
             d_before = np.abs(self.domain.barriers_position[barrier_index] - initPos[interacting])
             crosses,modifiedStep = self.crossesBarrier(d_before, d_after, self.domain.permeability[barrier_index], self.domain.diffusivity[self.indices][interacting],
                                           self.domain.diffusivity[newIndices][interacting], transit_model)
-
-            pos_reflect = -1 * np.sign(step[interacting]) * modifiedStep + self.domain.barriers_position[barrier_index]
-            pos_pass = pos_interact
+            pos_reflect = -1 * np.sign(step[interacting]) * d_after + self.domain.barriers_position[barrier_index]
+            pos_pass = self.domain.barriers_position[barrier_index] + np.sign(step[interacting]) * (modifiedStep)
             pos_new = np.where(crosses, pos_pass, pos_reflect)
             new_indices = np.where(crosses, newIndices[interacting], self.indices[interacting])
             self.position[interacting] = pos_new
