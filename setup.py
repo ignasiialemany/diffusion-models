8cceda17ea796e972452ceb9548f25de234144f8
Jan Niklas Rose <janniklas.rose@gmail.com>
2020-08-29 18:19:29 +0200

    Combine analytical and mcrw modules into one

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(name='diffusion_models',
       author='Jan Niklas Rose',
       author_email='janniklas.rose@gmail.com',
       url='https://github.com/janniklasrose/diffusion-models',
-      packages=['analytical'],
+      packages=['diffusion'],
      )
