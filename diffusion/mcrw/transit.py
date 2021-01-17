ca8702416db50f0beb3c4d3a00a5c881aab93b6e
Jan Niklas Rose <janniklas.rose@gmail.com>
2021-01-17 23:56:20 +0000

    Add a few transit models

--- diffusion/mcrw/transit.py
+++ diffusion/mcrw/transit.py
@@ -23,3 +23,43 @@ class TransitABC(ABC):
         decision = uval <= p_t
         cls._crossing(decision, dx_i, dx_j, D_i, D_j)
         return decision
+
+
+class Constant(TransitABC):
+    """Transit model using a fixed probability.
+    Can be used to set all barriers to be impermeable (p_t=0),
+    fully permeable (p_t=1), or anything inbetween.
+    """
+
+    def __init__(self, p_t=0.5):
+        self.p_t = p_t
+
+    def probability(self, *args):
+        return self.p_t
+
+
+class Parrot(TransitABC):
+    """Transit model that repeats barrier permeability"""
+
+    @classmethod
+    def probability(cls, dx_i, dx_j, D_i, D_j, P):
+        return P
+
+
+class Maruyama2017(TransitABC):
+    """Interface model after Maruyama2017"""
+
+    @classmethod
+    def probability(cls, dx_i, dx_j, D_i, D_j, P):
+        p_t = np.minimum(np.sqrt(D_j/D_i), 1.0)
+        return p_t
+
+
+class Fieremans2010(TransitABC):
+    """Membrane model after Fieremans2010"""
+
+    @classmethod
+    def probability(cls, dx_i, dx_j, D_i, D_j, P):
+        term = 2*P*dx_i/D_i
+        p_t = np.where(np.isinf(P), 1, term/(1+term))
+        return p_t
