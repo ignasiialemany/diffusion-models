4882734195a66f82358748b614ff135b6b6b14d3
Jan Niklas Rose <janniklas.rose@gmail.com>
2021-01-18 16:01:07 +0100

    Increment version for first release

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='diffusion-models',
-      version='0.0',
+      version='0.1',
       description='Diffusion Models',
       author='Jan Niklas Rose',
       author_email='janniklas.rose@gmail.com',
