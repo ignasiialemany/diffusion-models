c9d31a26e82d82d551d5cfd838ae43a66d67e3b3
Jan Niklas Rose <janniklas.rose@gmail.com>
2021-01-22 13:56:26 +0100

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='diffusion-models',
-      version='0.1',
+      version='0.2',
       description='Diffusion Models',
       author='Jan Niklas Rose',
       author_email='janniklas.rose@gmail.com',
